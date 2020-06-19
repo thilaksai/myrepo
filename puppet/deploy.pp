1014cdce2a693aa9f512c6abc4078090a53a7707
prashant <pbeniwal@yahoo.com>
2020-06-19 13:12:49 +0530

    updated

--- puppet/deploy.pp
+++ puppet/deploy.pp
@@ -1,36 +1,36 @@
 class dockerwar::deploy {
 
-    file {'/home/edureka/docker':
+    file {'/home/devops/docker':
             ensure => directory,
             mode => '0777'
     }
 
-    file {'/home/edureka/docker/target':
+    file {'/home/devops/docker/target':
             ensure => directory,
             mode => '0777'
     }
 
-    file {'/home/edureka/docker/target/calculator.war':
+    file {'/home/devops/docker/target/calculator.war':
             mode => '0777',
             source => "puppet:///modules/dockerwar/calculator.war"
     }
 
-    file {'/home/edureka/docker/Dockerfile':
+    file {'/home/devops/docker/Dockerfile':
             mode => '0777',
             source => "puppet:///modules/dockerwar/Dockerfile"
     }
     exec {'Docker build':
-            cwd => '/home/edureka/docker/',
+            cwd => '/home/devops/docker/',
             command => '/usr/bin/docker build -t calculatorwithwar .'
     }
 
     exec {'Docker rm':
-            cwd => '/home/edureka/docker/',
+            cwd => '/home/devops/docker/',
             command => '/usr/bin/docker rm --force calculator8888 || echo'
     }
 
     exec {'Docker run':
-            cwd => '/home/edureka/docker/',
+            cwd => '/home/devops/docker/',
             command => '/usr/bin/docker run -d -p 8888:8080 --name=calculator8888 calculatorwithwar'
     }
 }
